01ca48fb72179e9a46d05923e5887ba9a310baa7
Daniel Munday <mundaymeister@gmail.com>
2019-01-23 18:45:58 +0000

    Updates

---
+++ precache-manifest.2c665b2dc3e0effa4a6ab473309fe0e4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2b79d1a6720bd4d077d0",
+    "url": "/brick-rpg/static/js/runtime~main.2b79d1a6.js"
+  },
+  {
+    "revision": "20c38488baeac294360e",
+    "url": "/brick-rpg/static/js/main.20c38488.chunk.js"
+  },
+  {
+    "revision": "924c4f14e22748ae63a9",
+    "url": "/brick-rpg/static/js/1.924c4f14.chunk.js"
+  },
+  {
+    "revision": "20c38488baeac294360e",
+    "url": "/brick-rpg/static/css/main.48f0ffce.chunk.css"
+  },
+  {
+    "revision": "c591f4c8a3c9dbcd968613263eb98679",
+    "url": "/brick-rpg/index.html"
+  }
+];
