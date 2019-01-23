3993e0a1e8130ec960ea3c28d6762547019d1717
Daniel Munday <mundaymeister@gmail.com>
2019-01-23 18:31:18 +0000

    github pages

---
+++ docs/precache-manifest.c1cc37709c7126ac9b56f64cbb163730.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b5c558aa746ce5269053",
+    "url": "/static/js/main.b5c558aa.chunk.js"
+  },
+  {
+    "revision": "924c4f14e22748ae63a9",
+    "url": "/static/js/1.924c4f14.chunk.js"
+  },
+  {
+    "revision": "b5c558aa746ce5269053",
+    "url": "/static/css/main.48f0ffce.chunk.css"
+  },
+  {
+    "revision": "733c3e268d0852c058f878f2a5cd2a80",
+    "url": "/index.html"
+  }
+];
